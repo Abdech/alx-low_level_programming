7df0db2664732e6caf5731213a6f85f537b2a00a
Abdulaziz Saad <abech33@gmail.com>
2022-10-19 17:34:07 -0700

    Print lowercase alphabets 10 tim

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,25 @@
 #include "main.h"
 #include "ctype.h"
 /**
- * print_alphabet - Prints alphabet in small letter
+ * print_alphabet_x10 - Prints alphabet
  *
  * Return: void
  */
-void  print_alphabet(void)
+void  print_alphabet_x10(void)
 {
 	int letters = 'A';
+	int i = 0;
 
-	while (letters <= 'Z')
+	while (i < 10)
 	{
+		while ( letters <= 'Z')
+		{
 		int lowerCase = tolower(letters);
 
 		_putchar(lowerCase);
 		letters++;
+		}
+			_putchar('\n');
+			i++;
 	}
-	_putchar('\n');
 }
