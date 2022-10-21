4121fb14dfaaa3bb47dfa713d949f4f072b407d7
Abdulaziz Saad <abech33@gmail.com>
2022-10-21 15:38:41 -0700

    Corrected the task 5
    It was printing about 11 lines which is not correct and also printing wrong output coz i was not checking for condition

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *more_numbers - prints 10times 0 to 14 o one line
  *
@@ -10,14 +9,15 @@ void more_numbers(void)
 {
 	int repeat = 0;
 
-	while (repeat <=  10)
+	while (repeat <  10)
 	{
 		int i = 0;
 
 		while (i <= 14)
 		{
-			_putchar((i / 10) + '0');
-		_putchar('0' +  (i % 10));
+			if (i > 10)
+				_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
 		i++;
 		}
 	_putchar('\n');
