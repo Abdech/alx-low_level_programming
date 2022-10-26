dd96a30fc4cef7ffc2b5d29b6b45a86acf92ba77
Abdulaziz Saad <abech33@gmail.com>
2022-10-25 17:15:52 -0700

    This is the first commit that takes a pointer as an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *0-reset_to_98.c
+ *
+ *n- is the pointer
+ *
+ *return - the functions returns a void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is the 5th project in this alx low level programming and base on pointers, arrays and strings 
+
