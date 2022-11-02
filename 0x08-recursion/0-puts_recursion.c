54f4aeda3ad87351781af43488525854154e5ce6
root <root@AzizSaad.localdomain>
2022-11-03 00:20:18 +0100

    The 8th project which is base on recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts_recursion - this function call itself until conditiion is met
+  *
+  *@s: is parameter that is being decrement
+  *
+  *Return: void is returned
+  */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
---
+++ 0x08-recursion/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _puts_recurion - this function call itself until conditiion is met
+  * 
+  *@s: is parameter that is being decrement
+  * 
+  *Return: void is returned
+  */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
+
