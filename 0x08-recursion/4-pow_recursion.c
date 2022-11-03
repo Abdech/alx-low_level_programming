80e0f0259fe768e4b25afa73dc9582d5564cef16
root <root@AzizSaad.localdomain>
2022-11-03 23:05:55 +0100

    Added a function that recursively return the value x raised to power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _pow_recursion - this function returns the value of
+  * raise to the power of y.
+  *
+  *@x: is parameter that is the base
+  *@y: is parameter that is raised by
+  *
+  *Return:returns an int
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (1);
+}
