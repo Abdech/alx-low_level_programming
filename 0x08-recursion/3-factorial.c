1387c58ac114f5828687eea51b629eb6824bce70
root <root@AzizSaad.localdomain>
2022-11-03 02:14:18 +0100

    Final correction to meet the requirment of the task of factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,10 +9,12 @@
   *Return:returns an int
   */
 
-int factorial(unsigned int n)
+int factorial(int n)
 {
-	if (n <= 1)
+	if (n < 0)
 		return (-1);
+	else if (n == 1)
+		return (1);
 	else
 		return (n * factorial(n - 1));
 }
