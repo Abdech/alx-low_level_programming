585eadd823e9efb4715e8c28a1f299f2041db9b0
root <root@AzizSaad.localdomain>
2022-11-05 00:02:33 +0100

    Task 3- the space prompted 5 errors
    Now I have correct and added a variable that holds the error.w

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d \n", result);
+		printf("%d\n", result);
 		return (0);
 	}
 }
