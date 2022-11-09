3bd8aac4be6bfa5e7698923ef540a9c62ab9367c
root <root@AzizSaad.localdomain>
2022-11-09 04:26:44 +0100

    Added task 1
    This function returns a ponter to a newly allocated space in memory with a
    copy from parameter wq

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strdup - this function returns a pointer to a newly allocated
+ *  space in memory, with duplicate of the parameter in d pointer
+ * @str: str that is duplicated and saved to pointer
+ *
+ * Return: an array.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	s = malloc(strlen(str) + 1 * sizeof(char));
+	if (str == NULL || str == " ")
+	{
+		return (NULL);
+	}
+	else if (s == NULL)
+		return (NULL);
+	{
+		strcpy(s, str);
+		return (s);
+	}
+}
