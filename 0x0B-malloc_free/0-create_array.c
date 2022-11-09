e7d9a92a54a8e9c2a21e8b3c4a98fac7a6f9ac69
root <root@AzizSaad.localdomain>
2022-11-09 02:55:48 +0100

    Project 0x0B-malloc_free: Task 0
    The task requires me to create an array of chars which initilizes it with a specific char
    and also return Null when size is zero

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - this functions creates an array of chars
+ * @size: the size of the array multiplied by size of chars
+ * @c: the character to be inserted
+ *
+ * Return: an array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *carray;
+	unsigned int i;
+
+	carray = malloc(sizeof(char) * size);
+	if (carray == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		carray[i] = c;
+	}
+	return (carray);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+This projects focuses on mallo and free
+
+That is memory allocation and freeing the allocated memory after ward
+
