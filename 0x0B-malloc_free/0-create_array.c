89bf2a36e5266894a12ad5532ec0c7980244c87e
root <root@AzizSaad.localdomain>
2022-11-09 03:11:16 +0100

    Still corrected the task 0
    I passed the check 8 but another error arise with check twelve
    I add condition that checks whether malloc is null to avoid problems

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 	carray = malloc(sizeof(char) * size);
 	if (size == 0)
 		return (NULL);
+	else if (carray == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		carray[i] = c;
