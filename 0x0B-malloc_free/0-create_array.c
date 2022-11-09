535e5010e08ec59e932c4a9b4284d90452a893d6
root <root@AzizSaad.localdomain>
2022-11-09 03:03:17 +0100

    Corrected task 0
    I failed check 8, I add char at the end of the loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	carray = malloc(sizeof(char) * size);
-	if (carray == NULL)
+	if (size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		carray[i] = c;
 	}
+	carray[i] = '\0';
 	return (carray);
 }
