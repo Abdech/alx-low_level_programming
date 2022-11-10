36cc14eae1abd30c658e3b61033ac49d80fc0d5e
root <root@AzizSaad.localdomain>
2022-11-10 01:19:41 +0100

    Corrected all the mistake
    I spent more 4 hours but couldn't wrap my head around the problem
    I got some explanation am able to solve it but with intensive guidewq

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,8 @@
 /**
  *str_concat - this function returns a pointer to a newly allocated
  *  space in memory, with duplicate of the parameter in d pointer
- * @str: str that is duplicated and saved to pointer
+ * @s1: first parameter which is str
+ * @s2: second parameter which is str that is added to s1
  *
  * Return: an array.
  */
@@ -14,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s;
 	int len_s1, len_s2, i, len;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -28,15 +29,9 @@ char *str_concat(char *s1, char *s2)
 	s = malloc((len + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	while ( i < len_s1)
-	{
+	for (i = 0; i < len_s1; i++)
 		s[i] = s1[i];
-		i++;
-	}
-	while (i < len_s2)
-	{
-		s[len_s1] = s2[i];
-		i++;
-	}
+	for (i = 0; i < len_s2; i++)
+		s[len_s1 + i] = s2[i];
 	return (s);
 }
