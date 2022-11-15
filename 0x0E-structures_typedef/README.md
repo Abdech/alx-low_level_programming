4c51957f74a54818d9e7e6044306b4cdf9fdbd3c
abdech <abdech33@gmail.com>
2022-11-15 23:03:19 +0100

    Added task 0 for project 0x0E-structures_typeofwq

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+This project 0x0E-structures-typedef
+
+The project teaches how use struct in c and typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - This is a dog little descr.
+ * @name: Name of the dog
+ * @age: Age of the dog 
+ * @owner: Owner of the dog
+ *
+ * Description: Longer description
+ */
+typeof struct dog 
+{
+	char * name; 
+	float age;  
+	char * owner;
+} dog;
