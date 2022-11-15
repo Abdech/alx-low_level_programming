397262c294dc1a854320e36ea3e959b78ed2aa11
abdech <abdech33@gmail.com>
2022-11-16 00:37:07 +0100

    Task 0 problem solved
    I was having issues with betty thats stopped and no checker to know the problem.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,22 @@
-#ifndef _STRUCT_
-#define _STRUCT_
+#ifndef _STRUCT_DOG_
+#define _STRUCT_DOG_
 
 /**
  * struct dog - This is a dog little descr.
  * @name: Name of the dog
- * @age: Age of the dog 
+ * @age: Age of the dog
  * @owner: Owner of the dog
  *
  * Description: Longer description
  */
-struct dog 
+typedef struct dog
 {
-	char *name; 
-	float age;  
+	char *name;
+	float age;
 	char *owner;
-};
+} dog_n;
 
 /**
- * @dog_t: Typedef for struct
+ * dog_n - Typedef for struct
  */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif /* _STRUCT_ */
+#endif /* _STRUCT_DOG_ */
