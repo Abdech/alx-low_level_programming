c25aa42e4f4283ef15c54961b167700657571db1
abdech <abdech33@gmail.com>
2022-11-17 04:33:05 +0100

    Project 0x0E-function_pointers, task 0
    The task simply prints name of user base on input and function
    called during the execusion.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - the function prints name
+ *
+ *@name: name to be printed
+ *@f: pointer function
+ *
+ *Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		(*f)(name);
+}
